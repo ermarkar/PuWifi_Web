2231815d79c8c3d7aceb9c4dd4d7f2b93f7b6c3f
Sunil Kumar <sukumar@quark.com>
2018-06-27 17:58:19 +0530

    Show navigation links as drop down on small screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as M from "materialize-css/dist/js/materialize";
 
 @Component({
   selector: "puwifi-app",
   templateUrl: "app.component.html"
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild("menuDropdown")
+  elDropdown: ElementRef;
+
+  ngAfterViewInit() {
+    let iDropdown = new M.Dropdown(this.elDropdown.nativeElement, {});
+  }
 }

9046cbd7081fe27b4e9dfe05ed807a75d1a43e7d
Sunil Kumar <sukumar@quark.com>
2018-06-28 17:54:12 +0530

    Mail form implemented

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MailService } from '../../services/common/mail.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as M from "materialize-css/dist/js/materialize";
 
 @Component({
   selector: "contact-us",
   templateUrl: "contact-us.component.html"
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+
+  contactUsForm: FormGroup;
 
   constructor(private mailService: MailService) { }
 
-  mailUs() {
-    let data;
-    this.mailService.sendMail(data).subscribe((response) => {
-      console.log("data");
+  ngOnInit() {
+    this.contactUsForm = new FormGroup({
+      name: new FormControl("", Validators.compose([
+        Validators.required
+      ])),
+      emailId: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("[^ @]*@[^ @]*")
+      ])),
+      message: new FormControl("", Validators.compose([
+        Validators.required
+      ]))
+    });
+  }
+
+  mailUs(data) {
+    this.mailService.sendMail(data).subscribe((response: any) => {
+      if (response.message) {
+        var toastHTML = 'Thanks for contacting us!!';
+        M.toast({ html: toastHTML, classes: "rounded" });
+        this.contactUsForm.reset();
+      } else {
+        var toastHTML = 'Something went wrong :(';
+        M.toast({ html: toastHTML, classes: "rounded" });
+      }
     });
   }
 }
